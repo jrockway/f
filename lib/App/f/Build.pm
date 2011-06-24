66328b3bd51ae88874099c9201c3d8175c9e154b
Jonathan Rockway <jon@jrock.us>
2011-06-24 13:51:37 +0200

    adding some stub steps

---
+++ lib/App/f/Build.pm
@@ -0,0 +1,37 @@
+package App::f::Build;
+# ABSTRACT: step to build a dist ("make" or equivalent)
+use Moose;
+use namespace::autoclean;
+
+with 'f::Step';
+
+has 'dist' => (
+    is       => 'ro',
+    isa      => 'f::Dist',
+    required => 1,
+);
+
+sub BUILD {
+    my $self = shift;
+    $self->add_dependency(
+        $self->dist->named_step('unpack'),
+        $self->dist->named_step('configure'),
+    );
+}
+
+sub execute {
+    my ($self, $deps) = @_;
+
+    my $dir = $deps->{ $self->dist->named_step('unpack') };
+
+    $self->tick(
+        message => join ' ', 'built', $self->dist->name, 'in', $dir,
+    );
+
+    $self->done(
+        $self->dist->named_step('build') => 1,
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
